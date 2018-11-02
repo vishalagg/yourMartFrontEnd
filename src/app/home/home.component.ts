76daa641e24b45418aee74bc05194a239275bfd0
vishalagg <vishalagg1994@gmail.com>
2018-11-02 13:02:04 +0530

    Products List is displaying:)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  products : any
+  constructor(private productService : ProductService) {
+   }
 
   ngOnInit() {
+    this.productService.getProducts().subscribe( products => {
+      this.products = products
+    })
+  }
+
+  getProducts() {
+
   }
 
 }
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '../../node_modules/@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  url: string = "http://localhost:8080"
+  constructor(private http: HttpClient) { }
+
+  getProducts() {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'authentication' : localStorage.getItem('token'),
+        'Content-Type':  'application/json'
+      })
+    };
+    return this.http.get(this.url+"/product",httpOptions)
+  }
 }
